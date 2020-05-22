1a76296979b1256bd0303615a4074b94d892b6db
AmmarMasood <ammar.masood98@gmail.com>
2020-05-23 00:59:12 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Draggable, { DraggableCore } from "react-draggable"; // Both at the same time
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      x: "",
+      y: "",
+      x2: "",
+      y2: ""
+    };
+  }
+
+  handleDrag = e => {
+    console.log(e);
+    this.setState({ ...this.state, x: e.screenX, y: e.screenY });
+  };
+  handleDrag2 = e => {
+    console.log(e);
+    this.setState({ ...this.state, x2: e.screenX, y2: e.screenY });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div
+          style={{
+            width: "80%",
+            height: "80%",
+            border: "10px solid black",
+            // top: "20%",
+            position: "absolute"
+            // left: "15%",
+            // transform: "translate(15%, 20%)"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Draggable
+            axis="both"
+            handle=".handle"
+            defaultPosition={{ x: 0, y: 0 }}
+            position={null}
+            grid={[25, 25]}
+            scale={1}
+            onStart={this.handleStart}
+            onDrag={this.handleDrag}
+            onStop={this.handleStop}
+            bounds="parent"
+          >
+            <div
+              className="handle"
+              style={{
+                border: "2px solid black",
+                padding: "50px",
+                cursor: "pointer",
+                width: "100px",
+                backgroundColor: "#B0E0E6"
+              }}
+            >
+              <div>Click and Drag Me</div>
+              <div>
+                x: {this.state.x}, y: {this.state.y}
+              </div>
+            </div>
+          </Draggable>
+          <Draggable
+            axis="both"
+            handle=".handle"
+            defaultPosition={{ x: 0, y: 0 }}
+            position={null}
+            grid={[25, 25]}
+            scale={1}
+            onStart={this.handleStart}
+            onDrag={this.handleDrag2}
+            onStop={this.handleStop}
+            bounds="parent"
+          >
+            <div
+              className="handle"
+              style={{
+                border: "2px solid black",
+                padding: "50px",
+                cursor: "pointer",
+                width: "100px",
+                backgroundColor: "#FFC0CB"
+              }}
+            >
+              <div>Click and Drag Me</div>
+              <div>
+                x: {this.state.x2}, y: {this.state.y2}
+              </div>
+            </div>
+          </Draggable>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
